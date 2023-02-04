6b137c68c2efef8d08d3bd4fead4a90042c8fb9d
Arno Erpenbeck <aemail@posteo.net>
2023-02-04 22:32:57 +0100

    More project setup

---
+++ .vscode/extensions.js
@@ -0,0 +1,12 @@
+{   
+    "recommendations": [ 
+      "esbenp.prettier-vscode",
+      "dbaeumer.vscode-eslint",
+      "svelte.svelte-vscode",
+      "zixuanchen.vitest-explorer",
+      "bradlc.vscode-tailwindcss",
+      "tamasfe.even-better-toml",
+      "editorconfig.editorconfig",
+      "christian-kohler.npm-intellisense"
+   ]
+} 
