aeb7980e8cb6727ec04be1a1ff8ca494a43d9750
Arno Erpenbeck <aemail@posteo.net>
2023-02-04 21:44:53 +0100

    Add SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<h1 class="text-3xl font-bold underline">
+    Welcome to SvelteKit with Commerce Layer on Netlify!
+</h1>
+
+<p>Visit these sites to read the documentation on...</p>
+<ul class="list-disc list-inside">
+    <li><a href="https://kit.svelte.dev">SvelteKit</a></li>
+    <li><a href="https://docs.netlify.com/">Netlify</a></li>
+    <li><a href="https://commercelayer.io/developers">Commerce Layer</a></li>
+    <li><a href="https://tailwindcss.com/docs/installation">Tailwind CSS</a></li>
+</ul>
+
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+</style>
