7b3999bd41b441b8e0e40aa6e1dee4da0fbd329c
Arno Erpenbeck <aemail@posteo.net>
2023-02-18 20:45:04 +0100

    webhooks payload

--- src/routes/api/webhooks/+server.ts
+++ src/routes/api/webhooks/+server.ts
@@ -5,11 +5,11 @@ import hmacSHA256 from 'crypto-js/hmac-sha256'
 
 export const POST: RequestHandler = async ({ request }) => {
     const signature = request.headers.get('x-commercelayer-signature')
-    console.log(signature)
+    console.log(`Signature: ${signature}`)
     const hash = hmacSHA256(JSON.stringify(request.body), import.meta.env.VITE_CL_SHARED_SECRET)
     const encode = hash.toString(CryptoJS.enc.Base64)
-    console.log(`signature valid: ${encode === signature}`)
+    console.log(`Signature valid: ${encode === signature}`)
     const payload = await request.json()
-    console.log(payload)
+    console.log(`Received ${payload.data?.id} of type ${payload.data?.type}`)
     return json('ok')
 }
