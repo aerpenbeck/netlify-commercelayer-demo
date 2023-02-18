5a375d1124bdd9e96a021128a35560f08e14cbd4
Arno Erpenbeck <aemail@posteo.net>
2023-02-18 21:13:06 +0100

    validate signature

--- src/routes/api/webhooks/+server.ts
+++ src/routes/api/webhooks/+server.ts
@@ -5,21 +5,18 @@ import hmacSHA256 from 'crypto-js/hmac-sha256'
 
 export const POST: RequestHandler = async ({ request }) => {
     const signature = request.headers.get('x-commercelayer-signature') || ''
-    console.log(`Signature: ${signature}`)
     const rawBodyText = await request.text()
 
-    const hash = hmacSHA256(JSON.stringify(rawBodyText), import.meta.env.VITE_CL_SHARED_SECRET)
+    const hash = hmacSHA256(rawBodyText, import.meta.env.VITE_CL_SHARED_SECRET)
     const encode = hash.toString(CryptoJS.enc.Base64)
     const validSignature = encode === signature
-    console.log(`Signature valid: ${validSignature}`)
-
-    const hash2 = hmacSHA256(rawBodyText, import.meta.env.VITE_CL_SHARED_SECRET)
-    const encode2 = hash2.toString(CryptoJS.enc.Base64)
-    const validSignature2 = encode2 === signature
-    console.log(`Signature valid: ${validSignature2}`)
-
+    //console.debug(`Signature '${signature}' is valid: ${validSignature}`)
+    if (!validSignature) {
+        return json('Invalid signature', { status: 401, statusText: 'Unauthorized' })
+    }
     const body = JSON.parse(rawBodyText)
     const eventData = body.data
     console.log(`Received ${eventData.id} of type ${eventData.type}`)
+
     return json('ok')
 }
